9027c93e9455658ef531d23a7ff47e56556f1f89
Thomas Frössman <thomasf@jossystem.se>
2020-05-29 11:28:56 +0200

    add vga text mode terminal colors

---
+++ .config-base/dynamic-colors/colorschemes/vga.sh
@@ -0,0 +1,55 @@
+# Solarized colorscheme "light"
+#
+# See the Solarized homepage for details:
+# http://ethanschoonover.com/solarized
+#
+
+black="#000000"
+blue="#0000AA"
+green="#00AA00"
+cyan="#00AAAA"
+red="#AA0000"
+magenta="#AA00AA"
+yellow="#AA5500"
+white="#AAAAAA"
+brblack="#555555"
+brblue="#5555FF"
+brgreen="#55FF55"
+brcyan="#55FFFF"
+brred="#FF5555"
+brmagenta="#FF55FF"
+bryellow="#FFFF55"
+brwhite="#FFFFFF"
+
+
+background="$black"
+foreground="$white"
+cursor="$white"
+mouse_background="$cyan"
+mouse_foreground="$brcyan"
+highlight="brmagenta"
+border="$black"
+
+ color0="$black"    # black
+ color8="$brblack"  # brblack
+
+ color1="$red"     # red
+ color9="$brred"  # brred
+
+ color2="$green"   # green
+color10="$brgreen"   # brgreen
+
+ color3="$yellow"  # yellow
+color11="$bryellow"   # bryellow
+
+ color4="$blue"    # blue
+color12="$brblue"  # brblue
+
+ color5="$magenta" # magenta
+color13="$bfmagenta"  # brmagenta
+
+ color6="$cyan"    # cyan
+color14="$brcyan"  # brcyan
+
+ color7="$white"  # white
+color15="$brwhite"  # brwhite
